7da2ab357f6575b2016e8fa80428730046cb54aa
tyler <tylercharawell@gmail.com>
2021-07-30 13:30:10 -0700

    commit tree 360759be6216170aac6cc8fac6d61a6752d86ea9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250} 
+    validates :summary, length: {maximum: 250} 
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    # validate :fiction_or_nonfiction
+
+    # def fiction_or_nonfiction
+    #     unless category.match?("Fiction" || "Non-Fiction")
+    #         errors.add(:category, "is not included in the list")
+    #     end
+    # end
 end
